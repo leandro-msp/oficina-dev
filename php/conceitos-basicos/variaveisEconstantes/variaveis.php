68539b51a0f2fb74326928f5a72a2f0b95a8bc00
leandro-msp <leandromarcjt@gmail.com>
2026-09-13 22:35:10 -0300

    variaveis

---
+++ php/conceitos-basicos/variaveisEconstantes/variaveis.php
@@ -0,0 +1,22 @@
+<?php 
+// como declarar uma variável e armazernar um valor em uma varíavel
+$nome = "Leandro Marc";
+
+// como recuperar um valor de uma varíavel
+
+echo $nome."\n"; // <-- o comando echo vai exibir o dado armazenado dentro da varíavel
+
+# TIPOS DE VARIÁVEIS
+
+$funcionario = "João"; // variavel do tipo String (caracteres alfanumérico)
+$salario = 900.00; // variavel do tipo float
+$idade =  28 ; // variavel do tipo inteiro
+$ativo = true; // variavel tipo boolean
+// Concatenação
+
+echo $funcionario." tem " .$idade." anos de idade e recebe ".$salario." reais.";
+$nome = "Cristiano";
+echo "<hr>";
+echo $nome; // <-- novo valor 
+
+
