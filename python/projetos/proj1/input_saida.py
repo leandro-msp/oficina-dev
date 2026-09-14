80435d1f7e6c04241c18e01324cdada7c1c96919
leandro-msp <leandromarcjt@gmail.com>
2026-09-14 17:01:29 -0300

    moved

--- python/projetos/proj1/input_saida.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-cidade = input("Digite onde você mora: ")
-hobbie = input("Digite seu passatempo favorito: ")
-
-print ("Meu nome é ",nome,", tenho",idade,"anos de idade e moro em",cidade,".")
-print ("Nas horas vagas eu gosto de ",hobbie)
