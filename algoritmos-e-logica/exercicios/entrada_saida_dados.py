56ccbe9b0d12d959c89930d8ce1347f533414405
leandro-msp <leandromarcjt@gmail.com>
2026-09-14 19:58:15 -0300

    exercício 1

---
+++ algoritmos-e-logica/exercicios/entrada_saida_dados.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite onde você mora: ")
+hobbie = input("Digite seu passatempo favorito: ")
+
+print ("Meu nome é ",nome,", tenho",idade,"anos de idade e moro em",cidade,".")
+print ("Nas horas vagas eu gosto de ",hobbie)
