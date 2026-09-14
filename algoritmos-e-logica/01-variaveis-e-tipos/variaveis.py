1a6d9a25b428cc3e4e6e99663634a06b2ef284e3
leandro-msp <leandromarcjt@gmail.com>
2026-09-14 19:56:23 -0300

    o que são variáveis

---
+++ algoritmos-e-logica/01-variaveis-e-tipos/variaveis.py
@@ -0,0 +1,28 @@
+x = "UNINOVE" 
+a = 10
+b = 8.6
+c = False
+print(type(x)) 
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+#print
+
+d = "Uninove"
+y = "Leandro"
+e = 10
+print ("Bem vindo ao mundo Phyton")
+print (e)
+print (y)
+print (d)
+
+print("Bem vindo ao mundo Python com "+y+"professor da " + d , " que é ", e)
+
+#input
+
+print("Dados do Aluno")
+nome = input("Informe seu nome: ")
+idade = input ("Informe sua idade : ")
+print (nome,"você tem ", idade, "anos")
