e195393a0a0e3d504b9c6d3bf34f118e416492f7
leandro-msp <leandromarcjt@gmail.com>
2026-09-14 19:58:40 -0300

    exercicio 2

---
+++ algoritmos-e-logica/exercicios/calcular_media.py
@@ -0,0 +1,10 @@
+#projeto 2 - cálculo de média
+aluno = input("Digite seu nome: ")
+nota1 = float(input("Qual foi sua primeira nota?:"))
+nota2 = float(input("Qual foi sua segunda nota?:"))
+
+media = (nota1+nota2)/2
+
+print("o Aluno",aluno,"obeteve a média",media)
+
+
