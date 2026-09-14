49eec29cb5bcaba6c8a4ddb969defeb2bbdc0677
leandro-msp <leandromarcjt@gmail.com>
2026-09-13 22:31:47 -0300

    sintaxe

---
+++ php/conceitos-basicos/comando-echo.php
@@ -0,0 +1,13 @@
+<?php  // < -- abertura tag php
+
+echo "Olá Mundo!!\n ";
+# echo -> exibe valores na tela, quer seja no browser ou no terminal
+# \n -> quebra de linha para sáidas no terminal
+
+echo "<br>"; // quebra de linha na saída pelo negador (php é intregado com o HTML)
+echo "Curso completo de PHP e MySQL + Projetos Práticos"
+?>
+
+<hr> <!-- quebra de linha e add linha -->
+
+<?= "<br> Echo Abreviado"; // este é um modelo curto de chamar o comando echo, porém não é muito utilizado ?>
