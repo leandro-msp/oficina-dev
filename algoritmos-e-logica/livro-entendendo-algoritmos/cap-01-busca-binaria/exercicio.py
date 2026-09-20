a3eed762ab8cc776c181389445297677aa69f61b
leandro-msp <leandromarcjt@gmail.com>
2026-09-20 01:38:22 -0300

    busca de nomes utilizando pesquisa binária

--- algoritmos-e-logica/livro-entendendo-algoritmos/cap-01-busca-binaria/exercicio.py
+++ algoritmos-e-logica/livro-entendendo-algoritmos/cap-01-busca-binaria/exercicio.py
@@ -9,7 +9,7 @@ nomes = [
 ]
 
 def pesquisaBinaria(lista,nome):
-    lista.sort() # vai deixar a lista em ordem alfabética
+    lista.sort() # vai deixar a lista em ordem alfabética || relembrando que pesquisa binária so funciona com série de elementos ordenados (abc, 0123)
     começo = 0
     final = len(lista) -1
 
@@ -26,9 +26,9 @@ def pesquisaBinaria(lista,nome):
     return None
 
 procurarNome = input("Digite um nome que deseja procurar na lista: \n").capitalize() # usuário digita o elemento que deseja procurar , e tratamento de primeira letra maíscula, caso o escreva tudo minúsculo
-elemento = pesquisaBinaria(nomes,procurarNome) # o valor final retorna será um número que é o indice, ou None, e será armazenado na variável "elemento"
+elemento = pesquisaBinaria(nomes,procurarNome) # a variável elemento chama a função pesquisaBinaria e guarda para o si o resultado da busca(return)
 
 if elemento != None: # caso o resultado da variável for diferente de None, obviamente será um número, pois localizou na lista, e vai exbir que foi encontrado
     print (f"o nome {procurarNome} existe na lista e está no índice:",elemento,"\n")
 else:
-    print("O Nome procurado não existe na lista\n") # caso não haja o nome, elemento é igual a None , ou seja a condição já cai no falso ( elemento nn é diferente de None), ou seja nome não encontrado
+    print("O Nome procurado não existe na lista\n") # caso o valor procurado não esteja na lista, elemento == None , ou seja a condição já cai no falso ( elemento nn é diferente de None), ou seja nome não encontrado
