c33d1d629e310f8f64282683fc3aa9119102768c
leandro-msp <leandromarcjt@gmail.com>
2026-09-26 00:50:00 -0300

    antecessorSucessor

---
+++ algoritmos-e-logica/exercicios/antecessorSucessor.py
@@ -0,0 +1,10 @@
+#Antecessor e Sucessor
+numero = int(input("Digite um número: "))
+
+antecessor = numero-1
+sucessor = numero+1
+
+print(f"\nO número digitado foi: {numero}")
+print(f"Seu antecessor é: {antecessor}")
+print(f"Seu sucessor é: {sucessor}")
+
